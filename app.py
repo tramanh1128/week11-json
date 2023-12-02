5a14267e8df2c93fc127a9c107dd5edc1e7a8c4a
tramanh1128 <90444352+tramanh1128@users.noreply.github.com>
2023-12-02 17:49:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,19 @@
-from flask import Flask, request, session
-import datetime
-import uuid
+from flask import Flask, render_template, 
+request
+from markupsafe import escape
 import matplotlib.pyplot as plt
-
-
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-show_time = []
-
-@app.route('/access_times')
-def index():
-    # Get the visitor's IP address
-    ip_address = request.remote_addr
-
-    # Generate a unique token for this page load
-    page_token = str(uuid.uuid4())
-
-    # Record the access time in the session
-    current_time = datetime.datetime.now()
-    access_times = session.get('access_times', [])
-    access_times.append((page_token, ip_address, current_time))
-    session['access_times'] = access_times
-
-    # Display the access time to the visitor
-    formatted_time = current_time.strftime('%a %b %d %Y %H:%M:%S')
-    
-    show_time.append(formatted_time)
-    response = "<h2>Access Times:</h2>"
-    for x in show_time:
-        response += f"<p>{x}</p>"
-    return response
-    
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
+@app.route('/num_start')
+def num_start():
+    return render_template('plot.html');
+@app.route('/numbers',methods=['POST'])
+def numbers():
+    data = request.form['numbers']
+    print(type(data))
+    print(data)
+    ypoints = data.split(',')
+    plt.clf()
+    plt.plot(ypoints, marker = 'o')
+    fname = './static/linechart.jpg'
+    plt.savefig(fname)
+    return fname
